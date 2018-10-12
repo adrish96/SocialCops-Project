989a1ecb3f4be4fcf00a2ce6a9266cd19acae80d
adrish96 <ghoshadrish96@gmail.com>
2018-10-12 21:09:26 +0530

    added some functionalities

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 import os
 import subprocess
-#ip=input("enter IP of server: ")
-ip="192.168.102.150"
 
-var=os.popen("sudo showmount -e {}".format(ip))
+ip=input("enter IP of server: ")
+
+var=os.popen("sudo showmount -e {}".format(ip))  #show folder shared on the server
 var1=var.read()
 point=var1.splitlines()
-mountpoint=point[1][:-2]
-#userpath="/root/Desktop/nfsshare"
-userpath=input("enter folder to sync: ")
-subprocess.call("sudo mount -t nfs {}:{} {}".format(ip,mountpoint,userpath),shell=True)
+mountpoint=point[1][:-2]  #parsing the path of the folder on the server
 
+#userpath="/root/Desktop/nfsshare"
+
+userpath=input("enter folder to sync(it should be an empty folder): ")
+if userpath is []:
+	subprocess.call("sudo mount -t nfs {}:{} {}".format(ip,mountpoint,userpath),shell=True)
+	print("folder is mounted successfully and is now in sync!")
+else:
+	print("the folder is not empty, please try again.")
 
 
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import subprocess
 import os 
-subprocess.run(["mkdir","/root/Desktop/nfs"])
-subprocess.call("echo '/root/Desktop/nfs  *(rw,no_root_squash)' >> /etc/exports" , shell=True)
-var=subprocess.call("systemctl restart nfs ",shell=True)
-if var==0:
-	print("nfs directory  successfully created and shared using nfs " )
-else :
-	print("some error " ) 
+
+path=input("enter the path of the directory to share: ")
+if not os.path.isdir(path):
+	print("can't find the directory specified, try again.")
+else:
+	#subprocess.run(["mkdir","/root/Desktop/nfs"])
+	subprocess.call("echo '{}  *(rw,sync,no_root_squash)' >> /etc/exports".format(path) , shell=True) 
+	var=subprocess.call("systemctl restart nfs ",shell=True)
+	if var==0:
+		print("nfs directory  successfully created and shared using nfs " )
+
